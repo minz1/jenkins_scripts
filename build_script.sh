ace3da05c7e5a0b3be1cc6489a1a67631730be88
minz1 <emerytang@gmail.com>
2019-08-07 12:38:53 -0500

    jenkins_scripts: intial commit of simple build scripts

---
+++ build_script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Simple build script.
+
+# $1 - Build Directory
+# $2 - Device
+# $3 - ROM Prefix
+# $4 - Build flavor
+# $5 - Make clean?
+# $6 - Target package
+# $7 - Repo sync?
+
+if [ $# -lt 7 ]; then
+    exit
+fi
+
+cd /var/lib/jenkins/android/"$1"
+
+export USE_CCACHE=1
+
+source build/envsetup.sh
+
+if [ "$7" = true ]; then
+    repo sync --force-sync -j4
+fi
+
+lunch "$3_$2-$4" -j4
+
+if [ "$5" = true ]; then
+    mka clean
+fi
+
+if [ "$1" == "twrp" ]; then
+    export ALLOW_MISSING_DEPENDENCIES=true
+fi
+
+mka "$6"
---
+++ copy_artifacts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# $1 - Build number
+# $2 - Workspace Directory
+# $3 - Device
+# $4 - Build Directory
+# $5 - Target package
+
+cd /var/lib/jenkins/android/"$4"/out/target/product/"$3"
+
+rm -rf $2/*
+
+if [ "$5" == "bacon" ]; then
+    for z in *.zip; do
+        if [[ "${z}" != *"ota"* ]]; then
+            cp -v "$z" "$2/${z%.zip}-$1.zip"
+        fi
+    done
+elif [ "$5" == "bootimg" ]; then
+    cp boot.img "$2/boot-$1.img"
+elif [ "$5" == "recoveryimage" ]; then
+    cp recovery.img "$2/recovery-$1.img"
+    cp recovery.tar "$2/recovery-$1.tar"
+fi
